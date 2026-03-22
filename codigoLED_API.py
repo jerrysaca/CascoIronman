dac0b36607014f8d13dba8ac7900f2f104d7e918
jerrysaca <gerardo.saca3@gmail.com>
2026-03-22 15:45:27 -0600

    Código de prueba para encender y apagar un LED desde el estado de la API desde la RaspberryPi

---
+++ codigoLED_API.py
@@ -0,0 +1,42 @@
+import gpiozero as gz
+from gpiozero import LED
+from time import sleep
+import http.client
+import json
+import math
+from urllib.parse import urlencode
+
+led = LED(21)
+# Peticion a la API
+conn = http.client.HTTPSConnection("uacj.ivancarvajal.org")
+payload_req = ''
+headers = {}
+
+while True:
+
+    try:
+        conn.request("GET", "/ords/uacj/ironman/Jarvis?equipo=Equipo%202", payload_req, headers)
+        res = conn.getresponse()
+        data = res.read()
+
+        if res.status == 200:
+            response_json = json.loads(data)
+            try:
+                payload_str = response_json["items"][0]['payload']
+                
+                if status == 0:
+                    led.off()
+                    print("LED OFF")
+                else:
+                    led.on()
+                    print("LED ON")
+                    
+            except (KeyError, IndexError) as e:
+                print("Error al acceder a los datos de la respuesta:", e)
+        else:         
+            print(f"Error en la solicitud: {res.status}")
+    except Exception as e:
+        print("Error al realizar la solicitud:", str(e))
+        sleep(5)  # Esperar antes de intentar nuevamente
+    finally:
+        conn.close()            
